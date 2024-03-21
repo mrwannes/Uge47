700e24bcfe49bbf3a90b627a57debb5488e24b47
Anders Reus Hansen <reusen@gmail.com>
2024-03-21 19:35:59 +0100

    Hvorfor virker det ikke

--- API/Controllers/GalleryController.cs
+++ API/Controllers/GalleryController.cs
@@ -10,9 +10,7 @@ namespace API.Controllers
         [HttpGet]
         public IActionResult GetImages()
         {
-            // Der er ingen BaseURI sat. Normalt er det http eller https med i request men da dette er en lokal fil på et drev er der ikke
-            // Find løsning
-            var imageFiles = Directory.GetFiles("C:\\Users\\reuse\\OneDrive\\Pictures\\2020-05").Select(Path.GetFileName);
+            var imageFiles = Directory.GetFiles("C:\\Users\\reuse\\Pictures\\dyr");
             return Ok(imageFiles);
         }
     }
